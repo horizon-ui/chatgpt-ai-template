2dec0e1fe75762ebb13f602f57817c7732e8ab54
Odran HUSSON <odran@webforger.fr>
2023-08-09 10:37:30 +0200

    :rocket: add chat history in local storage

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,99 +57,120 @@ export default function Chat(props: { apiKeyApp: string }) {
 		{ color: 'whiteAlpha.600' },
 	);
 
-		const handleCopy = (text) => {
-				navigator.clipboard.writeText(text)
-						.then(() => {
-								alert('Text copied to clipboard!');
-						})
-						.catch(err => {
-								alert('Failed to copy text. Try manually.');
-								console.error('Failed to copy text: ', err);
-						});
-		};
+	useEffect(() => {
+    if (typeof window !== 'undefined') {
+        const savedChatHistory = localStorage.getItem('chatHistory');
+        if (savedChatHistory) {
+            setChatHistory(JSON.parse(savedChatHistory));
+        }
+    }
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('chatHistory', JSON.stringify(chatHistory));
+	}, [chatHistory]);
 
 
-		const handleTranslate = async () => {
-				const apiKey = apiKeyApp;
-				setInputOnSubmit(inputCode);
 
-				const maxCodeLength = model === 'gpt-3.5-turbo' ? 60000 : 60000;
+	const clearChatHistory = () => {
+    localStorage.removeItem('chatHistory');
+    setChatHistory([]);
+	}
+	
 
-				if (!apiKeyApp?.includes('sk-') && !apiKey?.includes('sk-')) {
-						alert('Please enter an API key.');
-						return;
-				}
+	const handleCopy = (text) => {
+			navigator.clipboard.writeText(text)
+					.then(() => {
+							alert('Text copied to clipboard!');
+					})
+					.catch(err => {
+							alert('Failed to copy text. Try manually.');
+							console.error('Failed to copy text: ', err);
+					});
+	};
 
-				if (!inputCode) {
-						alert('Please enter your message.');
-						return;
-				}
 
-				if (inputCode.length > maxCodeLength) {
-						alert(
-								`Please enter code less than ${maxCodeLength} characters. You are currently at ${inputCode.length} characters.`,
-						);
-						return;
-				}
+	const handleTranslate = async () => {
+			const apiKey = apiKeyApp;
+			setInputOnSubmit(inputCode);
 
-				setChatHistory([...chatHistory, { type: 'user', message: inputCode }]);
-				setLoading(true);
-				const controller = new AbortController();
-				const body: ChatBody = {
-						inputCode,
-						model,
-						apiKey,
-				};
+			const maxCodeLength = model === 'gpt-3.5-turbo' ? 60000 : 60000;
 
-				const response = await fetch('/api/chatAPI', {
-						method: 'POST',
-						headers: {
-								'Content-Type': 'application/json',
-						},
-						signal: controller.signal,
-						body: JSON.stringify(body),
-				});
+			if (!apiKeyApp?.includes('sk-') && !apiKey?.includes('sk-')) {
+					alert('Please enter an API key.');
+					return;
+			}
 
-				if (!response.ok) {
-						setLoading(false);
-						alert(
-								'Something went wrong went fetching from the API. Make sure to use a valid API key.',
-						);
-						return;
-				}
+			if (!inputCode) {
+					alert('Please enter your message.');
+					return;
+			}
 
-				const data = response.body;
+			if (inputCode.length > maxCodeLength) {
+					alert(
+							`Please enter code less than ${maxCodeLength} characters. You are currently at ${inputCode.length} characters.`,
+					);
+					return;
+			}
 
-				if (!data) {
-						setLoading(false);
-						alert('Something went wrong');
-						return;
-				}
+			setChatHistory([...chatHistory, { type: 'user', message: inputCode }]);
+			setLoading(true);
+			const controller = new AbortController();
+			const body: ChatBody = {
+					inputCode,
+					model,
+					apiKey,
+			};
 
-				const reader = data.getReader();
-				const decoder = new TextDecoder();
+			const response = await fetch('/api/chatAPI', {
+					method: 'POST',
+					headers: {
+							'Content-Type': 'application/json',
+					},
+					signal: controller.signal,
+					body: JSON.stringify(body),
+			});
 
-				let fullResponse = "";
+			if (!response.ok) {
+					setLoading(false);
+					alert(
+							'Something went wrong went fetching from the API. Make sure to use a valid API key.',
+					);
+					return;
+			}
 
-				// Add a temporary bot message that we'll update in real-time
-				const tmpBotMessage = { type: 'bot', message: '' };
-				setChatHistory(prev => [...prev, tmpBotMessage]);
+			const data = response.body;
 
-				while (true) {
-						const { value, done } = await reader.read();
-						if (done) break;
+			if (!data) {
+					setLoading(false);
+					alert('Something went wrong');
+					return;
+			}
 
-						const chunkValue = decoder.decode(value);
-						fullResponse += chunkValue;
+			const reader = data.getReader();
+			const decoder = new TextDecoder();
 
-						// Update the temporary bot message in real-time
-						tmpBotMessage.message = fullResponse;
-						setChatHistory(prev => [...prev.slice(0, -1), tmpBotMessage]); // overwrite last message
-				}
+			let fullResponse = "";
 
-				setLoading(false);
-				setInputCode('');  // Clear the input value
-		};
+			// Add a temporary bot message that we'll update in real-time
+			const tmpBotMessage = { type: 'bot', message: '' };
+			setChatHistory(prev => [...prev, tmpBotMessage]);
+
+			while (true) {
+					const { value, done } = await reader.read();
+					if (done) break;
+
+					const chunkValue = decoder.decode(value);
+					fullResponse += chunkValue;
+
+					// Update the temporary bot message in real-time
+					tmpBotMessage.message = fullResponse;
+					setChatHistory(prev => [...prev.slice(0, -1), tmpBotMessage]); // overwrite last message
+			}
+
+			setLoading(false);
+			setInputCode('');  // Clear the input value
+	};
 
 
 
