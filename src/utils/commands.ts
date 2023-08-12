aad378cd633a43cba370e89435e78cfeeb43e253
Odran HUSSON <odran@webforger.fr>
2023-08-12 20:35:14 +0200

    🔄 Update commit command and ♻️ refactor commands to allow custom GPT usage

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -42,65 +42,74 @@ function saveApiKeyToLocalStorage(key: string) {
 }
 
 export const handleCommands = (
-  commandText: string,
-  setLoading: (loading: boolean) => void,
-  addBotMessageToChatHistory: (message: string) => void,
-  clearChatHistory: () => void
+	commandText: string,
+	setLoading: (loading: boolean) => void,
+	addBotMessageToChatHistory: (message: string) => void,
+	clearChatHistory: () => void
 ) => {
-  const [commandName, ...args] = commandText.split(' ');
+	const [commandName, ...args] = commandText.split(' ');
 
-  if (apiKey === null) {
-    addBotMessageToChatHistory('API key not found');
-    setLoading(false);
-    return;
-  }
+	if (apiKey === null) {
+			addBotMessageToChatHistory('API key not found');
+			setLoading(false);
+			return;
+	}
 
-  switch (commandName) {
-    case '/clearChat':
-      clearChatHistory();
-      break;
-    case '/chatGPT':
-      const gptMessage = args.join(' ');
-      fetchAndAddGPTResponse(gptMessage, addBotMessageToChatHistory);
-      break;
-		case '/commit':
-				const commitMessage = "Generate a short commit message with one emoji at the beginning for the following changes " + args.join(' ');
-				fetchAndAddGPTResponse(commitMessage, addBotMessageToChatHistory);
-				break;
-    case '/help':
-      addBotMessageToChatHistory(
-        commands.map((command) => `${command.name}: ${command.description}`).join('\n')
-      );
-      break;
-    default:
-      addBotMessageToChatHistory('Unknown command');
-  }
+	switch (commandName) {
+			case '/clearChat':
+					clearChatHistory();
+					break;
+			case '/commit':
+					handleCommitCommand(args, addBotMessageToChatHistory);
+					break;
+			case '/help':
+					addBotMessageToChatHistory(
+							commands.map((command) => `${command.name}: ${command.description}`).join('\n')
+					);
+					break;
+			default:
+					addBotMessageToChatHistory('Unknown command');
+	}
 
-  setLoading(false);
+	setLoading(false);
 };
 
-export async function fetchAndAddGPTResponse(
-  message: string,
-  addBotMessageToChatHistory: (message: string) => void
-) {
-  try {
-    const response = await fetch('/api/completionAPI', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ prompt: message, apiKey }), // Include the message and apiKey in the request body
-    });
 
-    if (!response.ok) {
-      throw new Error('Failed to fetch chat GPT');
-    }
+export function handleCommitCommand(args: string[], addBotMessageToChatHistory: (message: string) => void) {
+	let commitMessage = "Generate a short commit message with one emoji at the beginning for the following changes " + args.join(' ');
 
-    const data = await response.json();
-    const completion = data.completion;
-    addBotMessageToChatHistory(completion);
-  } catch (error) {
-    console.error(error);
-    addBotMessageToChatHistory('Error fetching from chat GPT');
-  }
+	handleChatGPTCommand(commitMessage, apiKey)
+			.then(completion => {
+					addBotMessageToChatHistory("Here's your commit command with the personalized commit message : \n\n```sh \n\n" + 'git commit -m "' + completion + '"\n\n ```');
+			})
+			.catch(error => {
+					console.error(error);
+					addBotMessageToChatHistory('Error generating commit message');
+			});
+}
+
+
+
+
+export async function handleChatGPTCommand(message: string, apiKey: string | null) {
+	try {
+			const response = await fetch('/api/completionAPI', {
+					method: 'POST',
+					headers: {
+							'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({ prompt: message, apiKey }), // Include the message and apiKey in the request body
+			});
+
+			if (!response.ok) {
+					throw new Error('Failed to fetch chat GPT');
+			}
+
+			const data = await response.json();
+			const completion = data.completion;
+			return completion
+	} catch (error) {
+			console.error(error);
+			return 'Error fetching from chat GPT'
+	}
 }
